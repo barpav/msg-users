eff11fa8e3ac6dd7075117f6a1125fdcb76fd4e1
pavel <p.baranushkin@gmail.com>
2023-07-18 01:33:32 +0400

    fix: error check

--- internal/data/validate_credentials.go
+++ internal/data/validate_credentials.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"crypto/md5"
+	"fmt"
 )
 
 type queryValidateCredentials struct{}
@@ -19,7 +20,12 @@ func (s *Storage) ValidateCredentials(ctx context.Context, userId, password stri
 	sum := md5.Sum([]byte(password))
 
 	rows, err := s.queries[queryValidateCredentials{}].QueryContext(ctx, userId, sum[:])
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to validate credentials: %w", err)
+	}
+
 	defer rows.Close()
 
-	return rows.Next(), err
+	return rows.Next(), nil
 }
